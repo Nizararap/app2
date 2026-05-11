65be8fdf669d275c3431d8d36e4d4b7c91663d0f
Nizararap <nizarardhian8@gmail.com>
2026-05-11 17:36:45 +0700

    Upload via GitDeploy

--- overlay-dex/app/src/main/java/com/overlay/OverlayView.java
+++ overlay-dex/app/src/main/java/com/overlay/OverlayView.java
@@ -184,7 +184,7 @@ public class OverlayView extends LinearLayout {
     private String sha256(String base) {
         try {
             MessageDigest digest = MessageDigest.getInstance("SHA-256");
-            byte[] hash = digest.digest(base.getBytes(StandardCharsets.UTF-8));
+            byte[] hash = digest.digest(base.getBytes(StandardCharsets.UTF_8));
             StringBuilder hexString = new StringBuilder();
             for (byte b : hash) {
                 String hex = Integer.toHexString(0xff & b);
