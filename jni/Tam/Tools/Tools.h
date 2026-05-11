2b42bf9edfbdf7adb9fc2b36fe468f1ad93759c2
Nizararap <nizarardhian8@gmail.com>
2026-05-11 17:03:35 +0700

    Delete jni/Tam/Tools/Tools.h

--- jni/Tam/Tools/Tools.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <jni.h>
-#include "Dobby/dobby.h"
-
-namespace Tools {
-    void Hook(void *target, void *replace, void **backup);
-    uintptr_t GetBaseAddress(const char *name);
-}
