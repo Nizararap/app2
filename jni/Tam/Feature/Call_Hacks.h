9115092611540f86614dc329d0fbebb88fd16333
Nizararap <nizarardhian8@gmail.com>
2026-05-11 17:02:53 +0700

    Delete jni/Tam/Feature/Call_Hacks.h

--- jni/Tam/Feature/Call_Hacks.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#define m_IL2CPPLIB OBFUSCATE("liblogic.so")
-extern uintptr_t m_IL2CPP;
-extern bool      g_Initialized;
-
-#include "GameClass.h"
-#include "mono.h"
-#include "Radar.h"
